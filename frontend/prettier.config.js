cf8f88a46c77ef5fa042b5678628edf5b18f8b0b
raf <rraf@tuta.io>
2024-07-10 14:52:24 +0100

    update: installing and configuring packages i plan on using

---
+++ frontend/prettier.config.js
@@ -0,0 +1,9 @@
+const options = {
+    arrowParens: "avoid",
+    singleQuote: false,
+    bracketSpacing: false,
+    endOfLine: "lf",
+    semi: false,
+    tabWidth: 4,
+    trailingComma: "all",
+};
