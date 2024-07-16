00f33a38751dbddaa51e0ac9e5d1cb16592ef60a
raf <rraf@tuta.io>
2024-07-16 12:08:08 +0100

    chore: made the article grid play nice on small width

--- frontend/src/components/Articles.tsx
+++ frontend/src/components/Articles.tsx
@@ -2,6 +2,7 @@
 
 import { gql, useQuery } from "@apollo/client";
 import { ProductCard } from "./ProductCard";
+import { SearchBar } from "./SearchBar";
 
 const ARTICLES = gql`
     query getArticles {
@@ -34,34 +35,32 @@ interface Data {
 }
 
 const style =
-    "bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center";
+    "bg-black-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center";
 
 const Grid = () => {
     const { loading, error, data } = useQuery<Data>(ARTICLES);
     data && console.log(data);
     return (
         <div>
+            <SearchBar />
             {data && (
                 <section
                     id="articles"
                     className="
                 grid 
-                grid-cols-3 
+                md:grid-cols-4 
                 relative
-                auto-rows-[300px]
+                auto-rows-auto
                 gap-4
-                my-10
                 "
                 >
                     {data.articles.data.map((item, i) => (
-                        <div
-                            key={i}
-                            className={`${style} ${i === 1 && "md:col-span-2"} ${i === 2 && "md:row-span-2"}`}
-                        >
-                            <h2 className="text-xl font-bold">
+                        <div key={i} className={`${style}`}>
+                            {/* className={`${style} ${i === 1 && "md:col-span-2"} ${i === 2 && "md:row-span-2"}`} */}
+                            <h2 className="text-lg font-bold py-5">
                                 {item.attributes.title}
                             </h2>
-                            <p className="text-gray-400">
+                            <p className="text-gray-400 text-sm">
                                 {item.attributes.summary}
                             </p>
                         </div>
